2c2d599d9785dfc8b0ec98fd4cb9e66960fb607d
ashimbasyal <ashimbasyal@gmail.com>
2021-09-30 17:16:49 +0545

    day 6

---
+++ line/line/lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late List<SalesData> _chartData;
+
+  @override
+  void initState() {
+    _chartData = getCharData();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      
+      child: Scaffold(
+        body: SfCartesianChart(
+          series: <ChartSeries>[
+            LineSeries<SalesData, double>(
+                dataSource: _chartData,
+                xValueMapper: (SalesData sales, _) => sales.year,
+                yValueMapper: (SalesData sales, _) => sales.sales,
+                dataLabelSettings: DataLabelSettings(isVisible: true))
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<SalesData> getCharData() {
+    final List<SalesData> charData = [
+      SalesData(2017, 25),
+      SalesData(2018, 12),
+      SalesData(2019, 24),
+      SalesData(2020, 18),
+      SalesData(2021, 30),
+    ];
+    return charData;
+  }
+}
+
+class SalesData {
+  SalesData(this.year, this.sales);
+  final double year;
+  final double sales;
+}
